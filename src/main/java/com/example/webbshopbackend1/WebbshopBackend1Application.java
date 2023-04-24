cdef0890d36b6e26cfcfe12120f772df0513ead2
ThunsOJ <oscar_jidaker@icloud.com>
2023-04-24 14:10:08 +0200

    Initial commit

---
+++ src/main/java/com/example/webbshopbackend1/WebbshopBackend1Application.java
@@ -0,0 +1,13 @@
+package com.example.webbshopbackend1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbshopBackend1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebbshopBackend1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webbshopbackend1/WebbshopBackend1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webbshopbackend1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbshopBackend1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
