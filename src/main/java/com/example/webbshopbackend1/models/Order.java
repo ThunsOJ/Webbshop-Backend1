49e23a39d3363e84e9aa87e9525c63c0903a6042
Lamers <gustav@henrikssons.org>
2023-04-24 15:56:46 +0200

    Order

---
+++ src/main/java/com/example/webbshopbackend1/models/Order.java
@@ -0,0 +1,42 @@
+package com.example.webbshopbackend1.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Order {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+//    @OneToMany(cascade = CascadeType.ALL)
+//    private List<OrderProduct> products = new ArrayList<>();
+
+//    @OneToMany
+//    @JoinColumn
+//    private Customer customer ;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Order order)) return false;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
