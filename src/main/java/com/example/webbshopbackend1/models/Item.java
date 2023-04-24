937b605be19cf35e4c6b82aaa751eb3b075681ce
ThunsOJ <oscar_jidaker@icloud.com>
2023-04-24 16:03:40 +0200

    Item model created. YEET!

---
+++ src/main/java/com/example/webbshopbackend1/models/Item.java
@@ -0,0 +1,33 @@
+package com.example.webbshopbackend1.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.Objects;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Item {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private double price;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Item item)) return false;
+        return Objects.equals(getId(), item.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
