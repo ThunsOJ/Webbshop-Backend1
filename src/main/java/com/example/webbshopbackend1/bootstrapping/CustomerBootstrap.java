9b3cea7199c77afbfe11a535d3edea388d8741dc
Melinda Walter <melinda_walter@hotmail.com>
2023-04-26 15:03:21 +0200

    customer bootstrap

--- src/main/java/com/example/webbshopbackend1/bootstrapping/CustomerBootstrap.java
+++ src/main/java/com/example/webbshopbackend1/bootstrapping/CustomerBootstrap.java
@@ -13,12 +13,15 @@ public class CustomerBootstrap {
 
     private final CustomerRepository customerRepository;
 
-    public void addCustomers() {
+    public List<Customer> addCustomers() {
         Customer customer1 = new Customer("Ann Al", "860327");
         Customer customer2 = new Customer("Ludde", "341202");
         Customer customer3 = new Customer("Hassan", "021005");
         Customer customer4 = new Customer("Gunilla", "650928");
 
-        customerRepository.saveAll(Arrays.asList(customer1, customer2, customer3, customer4));
+        List<Customer> cList = Arrays.asList(customer1, customer2, customer3, customer4);
+        customerRepository.saveAll(cList);
+
+        return cList;
     }
 }
