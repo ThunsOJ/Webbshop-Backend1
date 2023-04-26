198364b5e165d83be23d43e8fbba608c74475a8e
ruckel <kevin.dybeck@gmail.com>
2023-04-26 12:45:22 +0200

    added OrderProductRepository

---
+++ src/main/java/com/example/webbshopbackend1/repositories/OrderProductRepository.java
@@ -0,0 +1,6 @@
+package com.example.webbshopbackend1.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderProductRepository extends JpaRepository <OrderProductRepository, Long> {
+}
