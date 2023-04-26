7f1b1157cf76404b109b2d197208676a2f31ae0a
ThunsOJ <oscar_jidaker@icloud.com>
2023-04-26 14:58:08 +0200

    Product bootstrapping gjord

--- src/main/java/com/example/webbshopbackend1/bootstrapping/ProductBootstrap.java
+++ src/main/java/com/example/webbshopbackend1/bootstrapping/ProductBootstrap.java
@@ -2,26 +2,33 @@ package com.example.webbshopbackend1.bootstrapping;
 
 import com.example.webbshopbackend1.models.Product;
 import com.example.webbshopbackend1.repositories.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 
 
+import java.util.Arrays;
+import java.util.List;
+
+
+@AllArgsConstructor
 public class ProductBootstrap {
 
     final ProductRepository repo;
 
-    public ProductBootstrap(ProductRepository repo) {
-        this.repo = repo;
-        createProducts();
-    }
-    public void createProducts(){
+
+    @Bean
+    public List<Product> createProducts(){
         Product p1 = new Product("Wookpanna",99.99);
         Product p2 = new Product("Kexchoklad",867.99);
         Product p3 = new Product("Kalaspuffar",3.99);
         Product p4 = new Product("Pringles",233.99);
 
-        repo.save(p1);
-        repo.save(p2);
-        repo.save(p3);
-        repo.save(p4);
+        List<Product> pList = Arrays.asList(p1, p2, p3, p4);
+
+        repo.saveAll(pList);
+
+        return pList;
     }
+
 }
