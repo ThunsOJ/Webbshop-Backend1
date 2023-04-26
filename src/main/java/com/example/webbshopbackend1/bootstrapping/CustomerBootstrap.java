6fb78ea7236c25ccc49188e0862fc494bda7d124
Melinda Walter <melinda_walter@hotmail.com>
2023-04-26 14:57:20 +0200

    customer bootstrap

--- src/main/java/com/example/webbshopbackend1/bootstrapping/CustomerBootstrap.java
+++ src/main/java/com/example/webbshopbackend1/bootstrapping/CustomerBootstrap.java
@@ -1,4 +1,24 @@
 package com.example.webbshopbackend1.bootstrapping;
 
+import com.example.webbshopbackend1.models.Customer;
+import com.example.webbshopbackend1.repositories.CustomerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Arrays;
+import java.util.List;
+
+@AllArgsConstructor
 public class CustomerBootstrap {
+
+    private final CustomerRepository customerRepository;
+
+    public void addCustomers() {
+        Customer customer1 = new Customer("Ann Al", "860327");
+        Customer customer2 = new Customer("Ludde", "341202");
+        Customer customer3 = new Customer("Hassan", "021005");
+        Customer customer4 = new Customer("Gunilla", "650928");
+
+        customerRepository.saveAll(Arrays.asList(customer1, customer2, customer3, customer4));
+    }
 }
