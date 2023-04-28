1a5cec3e0a37a28a9f62be42d86b40e011d0e24b
ThunsOJ <oscar_jidaker@icloud.com>
2023-04-28 16:52:54 +0200

    Nu kan man shoppa loss

--- src/main/java/com/example/webbshopbackend1/controllers/ProductViewController.java
+++ src/main/java/com/example/webbshopbackend1/controllers/ProductViewController.java
@@ -1,16 +1,17 @@
 package com.example.webbshopbackend1.controllers;
 
+import com.example.webbshopbackend1.models.Orders;
 import com.example.webbshopbackend1.models.Product;
+import com.example.webbshopbackend1.repositories.CustomerRepository;
+import com.example.webbshopbackend1.repositories.OrderRepository;
 import com.example.webbshopbackend1.repositories.ProductRepository;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -19,32 +20,78 @@ import java.util.List;
 public class ProductViewController {
 
     private final ProductRepository repo;
+    private final List<Product> products = new ArrayList<>();
+
+    private final OrderRepository orderRepository;
+    private final CustomerRepository customerRepository;
+    private boolean isOrdered;
 
     @RequestMapping("/all")
     public String findAll(Model model){
+
+        if (isOrdered){
+            products.clear();
+        }
+
         List<Product> p = repo.findAll();
         model.addAttribute("allProducts", p);
         model.addAttribute("name", "Name");
         model.addAttribute("price", "Price");
+        model.addAttribute("buy", "Buy");
         model.addAttribute("productTitle", "All products");
+        model.addAttribute("cartTitle", "Items in cart");
+        model.addAttribute("allCLicked",products);
         return "showAllProducts";
     }
 
     @RequestMapping("/{id}")
-    public Product findById(@PathVariable Long id){
-        return repo.findById(id).get();
+    public String findById(@PathVariable Long id, Model model){
+        products.add(repo.findById(id).get());
+        return findAll(model);
     }
 
     @RequestMapping("/add")
     public String addProduct(Model model){
         return "addProduct";
     }
+    @RequestMapping("/order")
+    public String addOrder(Model model){
+        return "order";
+    }
+
+    @RequestMapping("/clear")
+    public String empty(Model model){
+        products.clear();
+        return findAll(model);
+    }
+
 
     @PostMapping("/create")
     public String create(@RequestParam String name, double price, Model model) {
         repo.save(new Product(name, price));
         return findAll(model);
     }
+
+
+    @PostMapping("/buy")
+    public String purchase(@RequestParam String fname, Model model){
+        orderRepository.save(new Orders(
+                customerRepository.findDistinctFirstByName(fname),
+                products
+        ));
+        model.addAttribute("name", "Name");
+        model.addAttribute("price", "Price");
+        model.addAttribute("buy", "Buy");
+        model.addAttribute("productTitle", "All products");
+        model.addAttribute("cartTitle", "Items in cart");
+        model.addAttribute("allCLicked",products);
+        model.addAttribute("fname", fname);
+
+        isOrdered = true;
+        return "orderCompleted";
+    }
+
+
 
 
 }
